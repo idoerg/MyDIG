fb120a9986b4b100de605801262d2331221f181e
Andrew Oberlin <andyoberlin@gmail.com>
2012-11-21 15:29:42 -0500

    Changed Chado to contain users and images

--- settings.py
+++ settings.py
@@ -10,15 +10,6 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'main',                      # Or path to database file if using sqlite3.
-        'USER': 'mycoplasma',                      # Not used with sqlite3.
-        'PASSWORD': '5i72c44u',                 # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    },
-    
     'chado': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'chado',
